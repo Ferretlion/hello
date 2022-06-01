1aebdbff035cb5e4cb604caf29b8da2009f456a9
Ferretlion <1023744469@qq.com>
2022-06-01 10:19:44 +0800

    first ros commit

---
+++ build/hhh/CMakeFiles/hello_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_pub.dir/src/hello_pub.cpp.o"
+  "/home/ros/.ssh/catkin_ws/devel/lib/hhh/hello_pub.pdb"
+  "/home/ros/.ssh/catkin_ws/devel/lib/hhh/hello_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/hhh/CMakeFiles/hello_sub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_sub.dir/src/hello_sub.cpp.o"
+  "/home/ros/.ssh/catkin_ws/devel/lib/hhh/hello_sub.pdb"
+  "/home/ros/.ssh/catkin_ws/devel/lib/hhh/hello_sub"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
